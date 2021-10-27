f3cd2a0a092cbc59a74820fe8788fdb35017f388
maciek_kucharski <56729054+Maciekkucharski@users.noreply.github.com>
2021-10-27 14:58:33 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,86 +1,78 @@
 #include <functional>
-#include <iomanip>
 #include <iostream>
 #include <list>
 #include <random>
 #include <vector>
-#include <math.h>
+#include "lab_2/lab2.h"
+#include <chrono>
 #define PI 3.14159265
-std::ostream& operator<<(std::ostream& o, std::vector<double> v)
-{
-    for (auto e : v) {
-        o << std::fixed << std::setprecision(5) << " " << e;
+
+
+using namespace std;
+
+random_device rd;
+mt19937 mt_generator(rd());
+
+
+int main() {
+    int input = 0;
+    cout << " 1: himmelblau, 2:Levy" << endl;
+    cin >> input;
+    double bottom, top;
+    cout << "Bottom Range:" << endl;
+    cin >> bottom;
+    cout << "Top Range:" << endl;
+    cin >> top;
+    uniform_real_distribution<double> hop_range = uniform_real_distribution<double>(bottom, top);
+    int iterations;
+    cout << "Iterations:" << endl;
+    cin >> iterations;
+
+
+    if (input == 1) {
+        uniform_real_distribution<> random(-5, 5);
+        vector<double> p0 = {
+                random(mt_generator),
+                random(mt_generator),
+        };
+        cout << p0[0]<< "|" << p0[1] <<endl;
+//        auto begin = std::chrono::high_resolution_clock::now();
+        auto result = simulated_annealing(himmelblau_function, himmelblau_domain, p0, iterations, [](int k) { return 1000.0 / k; }, hop_range);
+        cout << "f(" << result << ") = " << himmelblau_function(result) << endl;
+//        auto end = std::chrono::high_resolution_clock::now();
+//        auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin);
+
+//        printf("Time measured: %.3f seconds.\n", elapsed.count() * 1e-9);
+
+//        auto begin2 = std::chrono::high_resolution_clock::now();
+        auto point = hill_climbing(himmelblau_function, himmelblau_domain, p0, iterations, hop_range);
+        cout << "f(" << point << ") = " << himmelblau_function(point) << endl;
+//        auto end2 = std::chrono::high_resolution_clock::now();
+//        auto elapsed2 = std::chrono::duration_cast<std::chrono::nanoseconds>(end2 - begin2);
+
+//        printf("Time measured: %.3f seconds.\n", elapsed2.count() * 1e-9);
+    } else {
+        uniform_real_distribution<> random(-10, 10);
+        vector<double> p0 = {
+                random(mt_generator),
+                random(mt_generator),
+        };
+        cout << p0[0]<< "|" << p0[1] <<endl;
+//        auto begin = std::chrono::high_resolution_clock::now();
+        auto result = simulated_annealing(himmelblau_function, himmelblau_domain, p0, iterations, [](int k) { return 1000.0 / k; }, hop_range);
+        cout << "f(" << result << ") = " << himmelblau_function(result) << endl;
+//        auto end = std::chrono::high_resolution_clock::now();
+//        auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin);
+//
+//        printf("Time measured: %.3f seconds.\n", elapsed.count() * 1e-9);
+
+//        auto begin2 = std::chrono::high_resolution_clock::now();
+        auto point = hill_climbing(levy_function, levy_domain, p0, iterations, hop_range);
+        cout << "f(" << point << ") = " << levy_function(point) << endl;
+//        auto end2 = std::chrono::high_resolution_clock::now();
+//        auto elapsed2 = std::chrono::duration_cast<std::chrono::nanoseconds>(end2 - begin2);
+
+//        printf("Time measured: %.3f seconds.\n", elapsed2.count() * 1e-9);
     }
-    return o;
+
 }
-
-std::vector<double> operator+(std::vector<double> a, std::vector<double> b)
-{
-    for (int i = 0; i < a.size(); i++) {
-        a[i] += b[i];
-    }
-    return a;
-}
-
-
-std::vector<double> hill_climbing(std::function<double(std::vector<double>)> f, std::function<bool(std::vector<double>)> f_domain, std::vector<double> p0, int iterations, std::uniform_real_distribution<double> hop_range){
-    std::random_device rd;
-    std::mt19937 gen(rd());
-    auto p = p0;
-    std::uniform_int_distribution<> distrib(0, p.size()-1);
-    if (!f_domain(p)) throw std::invalid_argument("The p0 point must be in domain");
-    for (int i = 0; i < iterations; i++) {
-        std::vector<double> p2;
-        do{
-            p2 = p;
-            p2[distrib(gen)] += hop_range(gen);
-        }while(!f_domain(p2));
-        double y2 = f(p2);
-        if (y2 < f(p)) {
-            p = p2;
-        }
-    }
-    return p;
-}
-
-std::vector<double> simulated_annealing(
-        std::function<double(std::vector<double>)> f,
-        std::function<bool(std::vector<double>)> f_domain,
-        std::vector<double> p0,
-        int iterations,
-        std::function<double(int)> T,
-        std::uniform_real_distribution<double> hop_range)
-{
-    std::random_device rd;
-    std::mt19937 gen(rd());
-    auto p = p0;
-    auto p_global_best = p0;
-
-    std::uniform_real_distribution<> u_k(0.0, 1.0);
-    std::uniform_int_distribution<> distrib(0, p.size()-1);
-    if (!f_domain(p)) throw std::invalid_argument("The p0 point must be in domain");
-
-    for (int k = 0; k < iterations; k++) {
-        std::vector<double> p2;
-        do{
-            p2 = p;
-            p2[distrib(gen)] += hop_range(gen);
-        }while(!f_domain(p2));
-        double y2 = f(p2);
-        if (y2 < f(p)) {
-            p = p2;
-
-        } else {
-            double u = u_k(gen);
-            if (u < exp(-abs(f(p2) - f(p)) / T(k))) {
-                p = p2;
-            } else {
-            }
-        }
-        if (f(p) < f(p_global_best)) {
-            p_global_best = p;
-        }
-    }
-    return p_global_best;
-}
-
