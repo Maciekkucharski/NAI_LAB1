909cdba3f75938f9e1b09643b6f98a94eb8d4a02
maciek_kucharski <56729054+Maciekkucharski@users.noreply.github.com>
2021-10-27 14:59:09 +0200

    Update lab_2f.cpp

--- lab2/lab_2f.cpp
+++ lab2/lab_2f.cpp
@@ -23,17 +23,18 @@ std::vector<double> operator+(std::vector<double> a, std::vector<double> b)
 }
 
 
-std::vector<double> hill_climbing(std::function<double(std::vector<double>)> f, std::function<bool(std::vector<double>)> f_domain, std::vector<double> p0, int iterations, std::uniform_real_distribution<double> shift){
+std::vector<double> hill_climbing(std::function<double(std::vector<double>)> f, std::function<bool(std::vector<double>)> f_domain, std::vector<double> p0, int iterations, std::uniform_real_distribution<double> hop_range){
     std::random_device rd;
     std::mt19937 gen(rd());
     auto p = p0;
     std::uniform_int_distribution<> distrib(0, p.size()-1);
     if (!f_domain(p)) throw std::invalid_argument("The p0 point must be in domain");
     for (int i = 0; i < iterations; i++) {
-        auto p2 = p;
+        std::vector<double> p2;
         do{
-            p2[distrib(gen)] += shift(gen);
-        }while(f_domain(p2));
+            p2 = p;
+            p2[distrib(gen)] += hop_range(gen);
+        }while(!f_domain(p2));
         double y2 = f(p2);
         if (y2 < f(p)) {
             p = p2;
@@ -48,22 +49,23 @@ std::vector<double> simulated_annealing(
         std::vector<double> p0,
         int iterations,
         std::function<double(int)> T,
-        std::uniform_real_distribution<double> shift)
+        std::uniform_real_distribution<double> hop_range)
 {
     std::random_device rd;
     std::mt19937 gen(rd());
     auto p = p0;
-    auto s_global_best = p0;
+    auto p_global_best = p0;
 
     std::uniform_real_distribution<> u_k(0.0, 1.0);
     std::uniform_int_distribution<> distrib(0, p.size()-1);
     if (!f_domain(p)) throw std::invalid_argument("The p0 point must be in domain");
 
     for (int k = 0; k < iterations; k++) {
-        auto p2 = p;
+        std::vector<double> p2;
         do{
-            p2[distrib(gen)] += shift(gen);
-        }while(f_domain(p2));
+            p2 = p;
+            p2[distrib(gen)] += hop_range(gen);
+        }while(!f_domain(p2));
         double y2 = f(p2);
         if (y2 < f(p)) {
             p = p2;
@@ -75,10 +77,10 @@ std::vector<double> simulated_annealing(
             } else {
             }
         }
-        if (f(p) < f(s_global_best)) {
-            s_global_best = p;
+        if (f(p) < f(p_global_best)) {
+            p_global_best = p;
         }
     }
-    return s_global_best;
+    return p_global_best;
 }
 
